69d97956e426b9dcaf7d425381c144ccef47ec83
adityach0uhan <m4with6x@gmail.com>
2023-08-02 13:12:10 +0530

    added list update functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,52 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+
+  const [userInput, setUserInput] = useState('');
+  const [addTask, setTask] = useState([])
+
+  function captureInput(e) {
+    setUserInput(e.target.value)
+  }
+  function addTaskToList() {
+    setTask(userInput)
+  }
+
+
   return (
+
     <>
       <div className="main-container">
-        
-
-        <div className="heading"><p>Add Your Tasks</p></div>
-        <div className="input-box"> <input type="text" /> <button className='addItem-btn btn'><i class="fa-solid fa-plus"></i></button></div>
+        <div className="heading">Your Todo's</div>
+        <div className="input-box"> <input type="text" placeholder='Add your tasks' onChange={captureInput} /> <button className='addItem-btn btn' onClick={() => { addTaskToList;}} ><i class="fa-solid fa-plus"></i></button></div>
         <div className="lists">
           <ul>
-            <li><div className='user-task'>Task</div> <span> <button className=' btn edit-btn' ><i class="fa-solid fa-pen-to-square"></i></button> <button className=" btn delete-btn"><i class="fa-solid fa-trash"></i></button></span></li>
+            <li><div className='user-task'>{addTask}</div>
+              <span>
+                <button className=' btn edit-btn' >
+                  <i class="fa-solid fa-pen-to-square"></i>
+                </button>
+                <button className=" btn delete-btn">
+                  <i class="fa-solid fa-trash"></i>
+                </button>
+              </span>
+            </li>
           </ul>
         </div>
-       
-        
 
 
 
-            
+
+
+
 
 
 
       </div>
-      
+
 
 
     </>
