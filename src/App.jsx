60df4e28ac3d9f53d27d08be3a202b27f58dcf9f
adityach0uhan <m4with6x@gmail.com>
2023-08-03 15:54:34 +0530

    input box is fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css'
 
-
 function App() {
-
 
   const [userInput, setUserInput] = useState('');
   const [addTask, setTask] = useState([]);
@@ -18,44 +16,48 @@ function App() {
   //adding task to the list 
   function addTaskToList() {
     if (!userInput) {
-      return 
+      return alert("Please Enter Something")
     } else {
       const currentDate = new Date().getTime().toString();
       const listData = {
         id: currentDate,
         task:userInput
       }
-      console.log(listData)
       setTask([...addTask, listData]);
-      setUserInput('');
+      setUserInput("");
     }
 
   }
 
   // deleting the item form the list 
-  function deleteList(listid) {
-   
+  function deleteList(itemID) {
     let filteredItem = addTask.filter((currentElement) => {
-      return listid !== currentElement.id
+      return itemID !== currentElement.id
     })
     setTask(filteredItem)
-  
+  }
+
+
+  //updating the item in the list 
+
+  function updateList(itemID, task) {
+    setUserInput(task)
+   
+
   }
 
   return (
-
     <>
-     
       <div className="main-container">
         <div className="heading">Your Todo's</div>
-        <div className="input-box"> <input type="text" placeholder='Add your tasks' onChange={captureInput} /> <button className='addItem-btn btn' onClick={addTaskToList} ><i class="fa-solid fa-plus"></i></button></div>
+        <div className="input-box"> <input type="text" value={userInput} placeholder='Add your tasks' onChange={captureInput} /> <button className='addItem-btn btn' onClick={addTaskToList} ><i class="fa-solid fa-plus"></i></button></div>
         <div className="lists">
           <ul>
             {addTask.map((currentElement,index) => {
             return <>
               <li key={index}><div className='user-task'> <span className='serial-num'>{index +1 }</span>{currentElement.task}</div>
                 <span>
-                  <button className=' btn edit-btn' >
+                  <button onClick={()=> updateList(currentElement.id , currentElement.task)} className=' btn edit-btn' >
                     <i class="fa-solid fa-pen-to-square"></i>
                   </button>
                   <button onClick={() => { deleteList(currentElement.id) }} className=" btn delete-btn">
